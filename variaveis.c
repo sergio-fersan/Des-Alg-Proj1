9b60333ef780eb2b86addc4ec04b84662e037860
Sergio Fersan <sergio.fersan.sss@gmail.com>
2024-11-10 14:00:48 -0300

    apaguei o que nao ia usar

--- variaveis.c
+++ variaveis.c
@@ -6,7 +6,7 @@ typedef struct {
     float valor;
 } Variavel;
 
-float lerVarBin(const char *arquivo, const char *variavel){
+float lerVar(const char *arquivo, const char *variavel){
     FILE *arq = fopen(arquivo, "rb");
 
     Variavel var;
@@ -21,7 +21,7 @@ float lerVarBin(const char *arquivo, const char *variavel){
     return 0; // 0 se nn achou
 }
 
-float lerVarIntBin(const char *arquivo, const char *variavel){
+float lerVarInt(const char *arquivo, const char *variavel){
     FILE *arq = fopen(arquivo, "rb");
 
     Variavel var;
@@ -36,7 +36,7 @@ float lerVarIntBin(const char *arquivo, const char *variavel){
     return 0; // 0 se nn achou
 }
 
-void escVarBin(const char *arquivo, const char *variavel, float valor){
+void escVar(const char *arquivo, const char *variavel, float valor){
     FILE *arq = fopen(arquivo, "rb+"); // leitura e escrita ao mesmo tempo
 
     Variavel var;
@@ -61,73 +61,4 @@ void escVarBin(const char *arquivo, const char *variavel, float valor){
         fwrite(&var, sizeof(Variavel), 1, arq);
     }
     fclose(arq);
-}
-
-float lerVar(const char *arquivo, const char *variavel){
-    FILE *arq = fopen(arquivo, "r");
-
-    char linha[100];
-
-    while(fgets(linha, sizeof(linha), arq)){
-        char tmpVar[15];
-        float tmpVal;
-
-        if(sscanf(linha, "%[^ ] %f", tmpVar, &tmpVal) == 2){  // %[^ ] procura até o espaço, %d lê o valor da variável
-            if(strcmp(tmpVar, variavel) == 0){
-                fclose(arq);
-                return tmpVal;
-            }
-        }
-    }
-    return 0;
-}
-
-int lerVarInt(const char *arquivo, const char *variavel){
-    FILE *arq = fopen(arquivo, "r");
-
-    char linha[100];
-
-    while(fgets(linha, sizeof(linha), arq)){
-        char tmpVar[15];
-        int tmpVal;
-
-        if(sscanf(linha, "%[^ ] %d", tmpVar, &tmpVal) == 2){  // %[^ ] procura até o espaço, %d lê o valor da variável
-            if(strcmp(tmpVar, variavel) == 0){
-                fclose(arq);
-                return tmpVal;
-            }
-        }
-    }
-    return 0;
-}
-
-void escVar(const char *arquivo, const char *variavel, float valor){
-    FILE *arq = fopen(arquivo, "r");
-    
-    int achouVar = 0; // diz se achou a variavel no txt
-    char linha[100];
-    char buffer[200] = "";
-
-    while(fgets(linha, sizeof(linha), arq)){
-        char tmpVar[15];
-        float tmpVal;
-        
-        if(sscanf(linha, "%[^ ] %f", tmpVar, &tmpVal) == 2){
-            if(strcmp(tmpVar, variavel) != 0){ // se as variaveis forem dif
-                snprintf(buffer + strlen(buffer), sizeof(buffer) - strlen(buffer), "%s %f\n", tmpVar, tmpVal);
-            } else{
-                snprintf(buffer + strlen(buffer), sizeof(buffer) - strlen(buffer), "%s %f\n", tmpVar, valor);
-                achouVar = 1;
-            }
-        } else{
-            strncat(buffer, linha, sizeof(buffer) - strlen(buffer) - 1); // deixa a linha igual se nn for usar
-        }
-    } // nada disso ta escrevendo ainda, so armazenando em variavel
-    fclose(arq);
-
-    if(achouVar == 1){
-        arq = fopen(arquivo, "w");
-        fputs(buffer, arq);
-        fclose(arq);
-    }
 }
